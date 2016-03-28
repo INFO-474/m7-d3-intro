a7945d7a41d82a59b18bb13dac1058482448c65d
mkfreeman <michael.k.freeman@gmail.com>
2016-03-27 22:44:02 -0700

    Removed answers from exercise-3

--- exercise-3/js/main.js
+++ exercise-3/js/main.js
@@ -26,69 +26,33 @@ var exam3 = [
 // You'll have to wait for you page to load to assign events to the elements created in your index.html file
 $(function() {
   // Select SVG
-  var mySvg = d3.select('#my-svg').attr('height', 200);
 
   // Reusable draw function
-  var draw = function(data) {
-    // Bind data to svg
-    var rects = mySvg.selectAll('rect').data(data, function(d){return d.id});
 
-    // enter elements
-    rects.enter()
-         .append('rect')
-         .attr('width', 0);
+    // Bind data to selection of rects in your svg
+
+
+    // Enter rect elements
+
 
     // Entering and updating elements rects
-    rects.transition()
-         .duration(500)
-         .attr('width', function(d){return d.grade})
-         .attr('y', function(d){return d.id*40})
-         .attr('height', 30)
-         .attr('x', 20);
+
 
     // Transition a remove for exiting elements
-    rects.exit()
-         .transition()
-         .duration(500)
-         .attr('width', 0)
-         .remove();
+
 
     // Perform the same data-binding for text
-    var texts = mySvg.selectAll('text').data(data, function(d){return d.id});
+
 
     // enter elements
-    texts.enter()
-         .append('text')
+
 
     // Entering and updating elements rects
-    texts.attr('y', function(d){return d.id*40 + 19})
-         .text(function(d){return d.student})
-         .style("fill", 'white')
-         .attr('x', 23);
+
 
     // Transition a remove for exiting elements
-    texts.exit()
-         .transition()
-         .delay(500)
-         .remove();
-  };
+    
 
   // Assign a click event for buttons. Use switch/case to apply logic of which data to use.
-  $('button').on('click', function() {
-    var btn = $(this).attr("id");
-    console.log(btn, 'draw')
-    switch(btn){
-      case 'exam-1':
-        draw(exam1);
-        break;
-      case 'exam-2':
-        draw(exam2);
-        break;
-      case 'exam-3':
-        draw(exam3);
-        break;
-      default:
-        break;
-    };
-  })
+
 });
