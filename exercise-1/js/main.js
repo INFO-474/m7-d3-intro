d549ead3db2d4a6553c7112a1a00547030a02581
mkfreeman <michael.k.freeman@gmail.com>
2016-04-14 09:53:21 -0700

    Fixed issues from rebase conflicts

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -4,13 +4,29 @@
 $(function() {
 
   // Select the element with id `sandbox` and append a `div` element to it. Make sure to store the div in a JavaScript varaible
+  var myDiv = d3.select('#sandbox').append('div');
 
   // Create a variable `mySvg` by appending an `svg` element to your newly created `div`
+  var mySvg = myDiv.append('svg');
 
-  // Set both the width and height attributes of your `svg` to 300 
+  // Set both the width and height attributes of your `svg` to 300 (this could have been chained above)
+  mySvg.attr('width', 300)
+       .attr('height', 300);
 
   // Append a `circle` element to your `svg`, setting the properties noted in index.html
+  mySvg.append('circle')
+       .attr('r', 30)
+       .attr('cx', 75)
+       .attr('cy', 30)
+       .style('opacity', .5)
+       .style('fill', 'blue');
 
   // Append a `rect` element to your `svg`, setting the properties noted in index.html
-
+  mySvg.append('rect')
+       .attr('x', 70)
+       .attr('y', 60)
+       .attr('width', 10)
+       .attr('height', 250)
+       .style('opacity', .5)
+       .style('fill', 'orange');
 });
--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -15,22 +15,28 @@ var people2 = [
 // You'll have to wait for you page to load to assign events to the elements created in your index.html file
 $(function() {
   // Create a varaible myDiv by selecting the element with id my-div (this is already created for you).
-
+  var myDiv = d3.select('#my-div');
 
   // Create a variable `paragraphs` by selecting all  `p` elements in  `myDiv`, and binding you data  `people` to it.
   // Make sure to specify the `name` attribute as an identifying characteristic in your data-join.
-
+  var paragraphs = myDiv.selectAll('p')
+                        .data(people, function(d) {return d.name});
 
 
   // Add a paragraph to the div for each person in your dataset following instructions in the index.html file
-
+  paragraphs.enter() // Determine which elements are in the dataset but are not represented in the DOM (`enter`)
+            .append('p') // `append` a paragraph (`p`)element
+            .text(function(d) {return d.name}) // set the text of the paragraph to the `name` of the person (`text`)
+            .style('color', function(d) {return d.favColor}); // set the color to the `favColor` of the person (`color`)
 
   // Use `setTimeout` function to wait 3 seconds, then do the following
-
-    // Re-define your variable `paragraphs` by selecting all `p` elements in `myDiv`, and binding the data `people2` to it.
+  setTimeout(function(){
+    // Re-define your variable `paragraphs` by selecting all `p` elements in `myDiv`, and binding the data `people2` to it. 
     // Make sure to specify the `name` attribute as an identifying characteristic in your data-join.
-
+    var paragraphs = myDiv.selectAll('p').data(people2, function(d) {return d.name});
 
     // Determine which people are no longer represented in the data (`exit`), and transition their opacity to 0 and remove them.
+    paragraphs.exit().transition().duration(1000).style('opacity', 0).remove();
+  }, 3000);
 
 });
